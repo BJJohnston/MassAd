af7c7f1380229c13bbf39abb52365ee3855ec520
infero00o <brian.johnston96@gmail.com>
2018-02-14 19:25:21 +0000

    bridgefy init

--- app/src/main/java/com/example/infero00o/massad/MainActivity.java
+++ app/src/main/java/com/example/infero00o/massad/MainActivity.java
@@ -1,13 +1,74 @@
-package com.example.infero00o.massad;
 
-import android.support.v7.app.AppCompatActivity;
+package com.example.infero00o.massad;
+import com.example.infero00o.massad.R;
+import com.example.infero00o.massad.*;
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.bridgefy.sdk.client.Bridgefy;
+import com.bridgefy.sdk.client.BridgefyClient;
+import com.bridgefy.sdk.client.Device;
+import com.bridgefy.sdk.client.Message;
+import com.bridgefy.sdk.client.MessageListener;
+import com.bridgefy.sdk.client.RegistrationListener;
+import com.bridgefy.sdk.client.Session;
+import com.bridgefy.sdk.client.StateListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Bridgefy.initialize(getApplicationContext(), new RegistrationListener() {
+            @Override
+            public void onRegistrationSuccessful(BridgefyClient bridgefyClient) {
+                // Start Bridgefy
+                startBridgefy();
+            }
+
+            @Override
+            public void onRegistrationFailed(int errorCode, String message) {
+
+            }
+        });
     }
+
+    private void startBridgefy() {
+        Bridgefy.start(messageListener, stateListener);
+    }
+
+    private MessageListener messageListener = new MessageListener() {
+
+    };
+
+    StateListener stateListener = new StateListener() {
+    };
 }
