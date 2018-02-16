f53e6c13cca1bab40471790c1d99d029d43b79fb
BJJohnston <brian.johnston96@gmail.com>
2018-02-16 16:26:10 +0000

    listeners added

--- app/src/main/java/com/example/infero00o/massad/MainActivity.java
+++ app/src/main/java/com/example/infero00o/massad/MainActivity.java
@@ -1,7 +1,9 @@
 
 package com.example.infero00o.massad;
+
 import com.example.infero00o.massad.R;
 import com.example.infero00o.massad.*;
+
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
@@ -45,30 +47,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BluetoothAdapter btA = BluetoothAdapter.getDefaultAdapter();
+        if (!btA.isEnabled()){
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 0);
+        }
 
 
-        Bridgefy.initialize(getApplicationContext(), new RegistrationListener() {
+        Bridgefy.initialize(getApplicationContext(), "c567d3e3-cb18-4f5f-af10-ef5cf018aa3a", new RegistrationListener() {
             @Override
             public void onRegistrationSuccessful(BridgefyClient bridgefyClient) {
-                // Start Bridgefy
+                //super.onRegistrationSuccessful(bridgefyClient);
                 startBridgefy();
             }
 
             @Override
-            public void onRegistrationFailed(int errorCode, String message) {
-
+            public void onRegistrationFailed(int errorCode, String message){
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
             }
         });
+
+
     }
 
-    private void startBridgefy() {
+    public void startBridgefy(){
+        Toast.makeText(getApplicationContext(), "Bridgefy Start", Toast.LENGTH_LONG).show();
         Bridgefy.start(messageListener, stateListener);
     }
 
     private MessageListener messageListener = new MessageListener() {
 
+
+    };
+    private StateListener stateListener = new StateListener(){
+
     };
 
-    StateListener stateListener = new StateListener() {
-    };
 }
