b5719b248050ee466e4de02f531b628dcb13e89e
BJJohnston <brian.johnston96@gmail.com>
2018-02-20 13:57:46 +0000

    finished tree

--- app/src/main/java/com/example/infero00o/massad/connectedPeers.java
+++ app/src/main/java/com/example/infero00o/massad/connectedPeers.java
@@ -1,5 +1,7 @@
 package com.example.infero00o.massad;
 
+import java.util.List;
+
 /**
  * Created by Brian on 2/20/2018.
  */
@@ -9,15 +11,48 @@ public class connectedPeers<T> {
     private List<connectedPeers> children = null;
     private connectedPeers parent = null;
 
-public connectedPeers(T peers){
-    this.peers = peers;
-}
+    public connectedPeers(T peers) {
+        this.peers = peers;
+    }
 
-public void addChild(connectedPeers child){
-    child.setParent(this);
-    this.children.add(child);
-}
+    public void addChild(connectedPeers child) {
+        child.setParent(this);
+        this.children.add(child);
+    }
 
+    public void addChild(T peers) {
+        connectedPeers<T> newChild = new connectedPeers<>(peers);
+        newChild.setParent(this);
+        children.add(newChild);
+
+    }
+
+    public void addChildren(List<connectedPeers> children) {
+        for (connectedPeers t : children) {
+            t.setParent(this);
+        }
+        this.children.addAll(children);
+    }
+
+    public List<connectedPeers> getChildren() {
+        return children;
+    }
+
+    public T getPeers() {
+        return peers;
+    }
+
+    public void setPeers(T peers) {
+        this.peers = peers;
+    }
+
+    private void setParent(connectedPeers parent) {
+        this.parent = parent;
+    }
+
+    public connectedPeers getParent() {
+        return parent;
+    }
 
 
 }
